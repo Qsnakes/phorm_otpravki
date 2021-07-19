e53f39a877c2607088b02f28db85683ad495f8d8
Pakhotin Nikita <nikitanik40658@icloud.com>
2021-07-19 22:22:04 +0500

    Ну вроде все

--- sendmail.php
+++ sendmail.php
@@ -23,5 +23,52 @@
         $hand = 'Левая';
     }
 
-    //
+    //Тело письма
+    $body = '<h1>Встречайте мега письмо</h1>';
+
+    if(trim(!empty($_POST['name']))){
+        $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+    }
+
+    if(trim(!empty($_POST['email']))){
+        $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+    }
+
+    if(trim(!empty($_POST['hand']))){
+        $body.='<p><strong>Рука:</strong> '.$hand.'</p>';
+    }
+
+    if(trim(!empty($_POST['age']))){
+        $body.='<p><strong>Возраст:</strong> '.$_POST['age'].'</p>';
+    }
+
+    if(trim(!empty($_POST['message']))){
+        $body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+    }
+
+    //Прикручиваем файлы
+    if (!empty($_FILES['image']['tmp_name'])) {
+        //Путь загрузки файлов
+        $filePath = __DIR__ . '/files/' . $_FILES['image']['name'];
+        //Грузим файл
+        if (copy($_FILES['image']['tmp_name'], $filePath)) {
+            $fileAttach = $filePath;
+            $body.='<p><strong>Фото в приложении:</strong></p>';
+            $mail->addAttachment($fileAttach);
+        }
+    }
+
+    $mail->Body = $body;
+
+    //Отправляем 
+    if (!$mail->send()) {
+        $message = 'Ошибка отправки';
+    } else {
+        $message = 'Сообщение отправлено!';
+    }
+
+    $response = ['message' => $message];
+
+    header('Content-type: application/json');
+    echo json_encode($response);
 ?>
